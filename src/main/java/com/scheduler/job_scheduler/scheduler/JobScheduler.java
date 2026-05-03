b1ca85f3bc0fe857f69de3ee2280d196d6c59be2
Aditya <aditya1115.ar@gmail.com>
2026-05-03 19:40:16 +0530

    Add JobScheduler, AKA Scheduler Engine (Core System)
    runs every few seconds
    fetches runnable jobs
    executes them in parallel
    updates status correctly

---
+++ src/main/java/com/scheduler/job_scheduler/scheduler/JobScheduler.java
@@ -0,0 +1,58 @@
+package com.scheduler.job_scheduler.scheduler;
+
+import com.scheduler.job_scheduler.model.Job;
+import com.scheduler.job_scheduler.model.JobStatus;
+import com.scheduler.job_scheduler.repository.JobRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Component
+@RequiredArgsConstructor
+public class JobScheduler {
+    private final JobRepository jobRepository;
+
+    // Thread pool for parallel execution
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    @Scheduled(fixedDelay = 5000)
+    @Transactional
+    public void scheduleJob() {
+        List<Job> jobs = jobRepository.findByStatusAndScheduleTimeBefore(
+                JobStatus.CREATED,
+                LocalDateTime.now()
+        );
+
+        for(Job job: jobs){
+            // mark as scheduled to avoid duplicate pickup
+            job.setStatus(JobStatus.SCHEDULED);
+            jobRepository.save(job);
+
+            executor.submit(() -> executeJob(job));
+        }
+    }
+
+    private void executeJob(Job job){
+        try {
+            job.setStatus(JobStatus.RUNNING);
+            jobRepository.save(job);
+
+            System.out.println("Executing job: " + job.getJobName());
+
+            // simulate work
+            Thread.sleep(3000);
+
+            job.setStatus(JobStatus.SUCCESS);
+        } catch (Exception e){
+            job.setStatus(JobStatus.FAILED);
+        }
+
+        jobRepository.save(job);
+    }
+}
